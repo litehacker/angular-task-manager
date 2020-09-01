d62b94fc9aca33c38cf93176556c78e189828d2a
Giorgi Gvimradze <giorgigvimradze@Giorgis-MacBook-Pro.local>
2020-09-01 18:28:49 +0400

    first commit with pages/taskview

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Task Manager Web App';
+  owner = 'Giorgi Gvimradze';
+  date = new Date();
+
+  constructor(){
+    console.log('this is a constructor call.');
+  }
+
+  getOwner():string{
+    return this.owner;
+  }
+
+}
